32b664a31cf0cb51533be1509dd1642fe2da759c
PTRBRNR <sidekk@gmail.com>
2023-07-13 10:28:09 +0200

    :tada:

---
+++ script.js
@@ -0,0 +1,6 @@
+const dayInput = document.createElement("input");
+const monthInput = document.createElement("input");
+const yearInput = document.createElement("input");
+
+const inputField = document.querySelector(".inputfield");
+inputField.append(dayInput, monthInput, yearInput);
